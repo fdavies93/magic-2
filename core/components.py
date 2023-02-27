ecddf5b51cc6fb9d6a658808281d0322eb2b4a66
Frank Davies <fdavies93@gmail.com>
2023-02-27 13:50:56 +0800

    Made get functions in Components take str or type arguments

--- core/components.py
+++ core/components.py
@@ -1,9 +1,11 @@
+from typing import Union
+from abc import ABC
 from core.registry import Registry
 
 # Should data be a straight dict or a dataclass?
 # Former is easier to use; latter allows code completion and better self-documenting behaviour.
 # Or should component be an ABC like Unity's Monobehaviour?
-class Component:
+class Component(ABC):
     def __init__(self, obj_id : int, type : str, data : dict):
         self.obj_id = obj_id
         self.type = type
@@ -21,7 +23,7 @@ class Component:
 class Components:
     def __init__(self):
         # type is primary because the main consumer is systems which operate over types 
-        self.components : dict[str, list[Components]] = {}
+        self.components : dict[str, list[Component]] = {}
         self.by_id : dict[int, list[Component]] = {}
         self.cur_id : int = 0
 
@@ -33,14 +35,20 @@ class Components:
         self.components[component.type].append(component)
         self.by_id[component.obj_id].append(component)
 
-    def on_object(self, id : int, type : str = None):
+    def disambiguate_type(self, type_of : Union[str, type]) -> str:
+        type_str = type_of
+        if not isinstance(type_str, str):
+            type_str = type_str.__name__
+        return type_str
+
+    def on_object(self, id : int, type_of : Union[str, type] = None):
         comps = self.by_id[id]
-        if type != None:
-            comps = list(filter(lambda comp : comp.type == type, comps))
+        if type_of != None:
+            comps = list(filter(lambda comp : comp.type == self.disambiguate_type(type_of), comps))
         return comps
 
-    def of_type(self, type : str) -> list[Component]:
-        return self.components[type]
+    def of_type(self, type_of : Union[str, type]) -> list[Component]:
+        return self.components[self.disambiguate_type(type_of)]
     
     def get_next_id(self) -> int:
         to_return = self.cur_id
